9e935bdaab9146f94b231bc72c33259db8178590
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-21 03:42:33 -0300

    test

--- rds.tf
+++ rds.tf
@@ -17,10 +17,12 @@ module "db" {
   username = "admin"
   port     = 3306
 
-  multi_az               = true
-  db_subnet_group_name   = module.vpc[0].database_subnet_group
-  vpc_security_group_ids = [module.mysql-sg[*].security_group_id]
-
+  multi_az             = true
+  db_subnet_group_name = module.vpc[0].database_subnet_group
+  vpc_security_group_ids = tolist([
+    for sg in module.mysql-sg : sg.security_group_id
+  ])
+  
   deletion_protection = false
 
   tags  = local.tags
