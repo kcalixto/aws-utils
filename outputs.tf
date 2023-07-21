c9ac54ddb655086f2bd714e1b6296178a71502e3
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-21 20:08:23 -0300

    following tf recomendations even without knowing why it's fucking forcing a plan error?

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "vpc_id" {
-  sensitive = false
+  sensitive = true
   value     = module.vpc.vpc_id
 }
 output "database_subnet_group" {
-  sensitive = false
+  sensitive = true
   value     = module.vpc.database_subnet_group
 }
 output "mysql_sg" {
-  sensitive = false
+  sensitive = true
   value     = aws_security_group.mysql-sg.id
 }
 output "rds" {
-  sensitive = false
+  sensitive = true
   value     = module.db
 }
