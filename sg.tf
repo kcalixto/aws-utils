1d39420fc2dbd9ddb2ed849c316a12c840ac2c1f
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-21 00:39:24 -0300

    added full rds + vpc

---
+++ sg.tf
@@ -0,0 +1,20 @@
+module "mysql-sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name   = "mysql-sg"
+  vpc_id = module.vpc.vpc_id
+
+  # ingress
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from within VPC"
+      cidr_blocks = module.vpc.vpc_cidr_block
+    },
+  ]
+
+  tags   = local.tags
+  deploy = local.deploy_mysql_sg
+}
---
+++ vpc.tf
@@ -0,0 +1,109 @@
+locals {
+  network_acls = {
+    public_inbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number     = 140
+        rule_action     = "allow"
+        from_port       = 80
+        to_port         = 80
+        protocol        = "tcp"
+        ipv6_cidr_block = "::/0"
+      },
+    ]
+    public_outbound = [
+      {
+        rule_number = 100
+        rule_action = "allow"
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 110
+        rule_action = "allow"
+        from_port   = 443
+        to_port     = 443
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 120
+        rule_action = "allow"
+        from_port   = 3306
+        to_port     = 3306
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+      {
+        rule_number = 130
+        rule_action = "allow"
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_block  = "0.0.0.0/0"
+      },
+    ]
+  }
+}
+
+module "vpc" {
+  name   = "${local.name}-general-vpc"
+  source = "terraform-aws-modules/vpc/aws"
+
+  cidr               = "10.0.0.0/16"
+  azs                = ["sa-east-1a", "sa-east-1b"]
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
+  database_subnets   = ["10.0.3.0/24"]
+  private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = false
+
+  public_dedicated_network_acl = true
+  public_inbound_acl_rules     = local.network_acls["public_inbound"]
+  public_outbound_acl_rules    = local.network_acls["public_outbound"]
+
+  database_subnet_names        = ["mysql_db_subnet"]
+  create_database_subnet_group = true
+
+  tags   = local.tags
+  deploy = local.deploy_vpc
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = module.vpc.vpc_id
+
+  tags   = local.tags
+  deploy = local.deploy_vpc
+}
