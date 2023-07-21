d939466e4d2a4753a65fcf393c682fbc1703cd21
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-20 23:06:21 -0300

    added internet gateway

--- provider.tf
+++ provider.tf
@@ -9,19 +9,3 @@ locals {
     GithubRepo = "aws-utils"
   }
 }
-
-################################################################################
-# VPC Module
-################################################################################
-
-module "vpc" {
-  name   = "${local.name}-general-vpc"
-  source = "terraform-aws-modules/vpc/aws"
-
-  cidr               = "10.0.0.0/16"
-  azs                = ["sa-east-1a", "sa-east-1b"]
-  private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
-  enable_nat_gateway = false
-
-  tags = local.tags
-}
---
+++ vpc.tf
@@ -0,0 +1,16 @@
+module "vpc" {
+  name   = "${local.name}-general-vpc"
+  source = "terraform-aws-modules/vpc/aws"
+
+  cidr               = "10.0.0.0/16"
+  azs                = ["sa-east-1a", "sa-east-1b"]
+  private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = false
+
+  tags = local.tags
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = module.vpc.vpc_id
+  tags = local.tags
+}
