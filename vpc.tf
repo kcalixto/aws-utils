d6d35c63ac6efe34ab7d7961e2676f350f6fb40e
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-20 23:29:50 -0300

    added public subnets

--- vpc.tf
+++ vpc.tf
@@ -4,6 +4,7 @@ module "vpc" {
 
   cidr               = "10.0.0.0/16"
   azs                = ["sa-east-1a", "sa-east-1b"]
+  public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = false
 
@@ -12,5 +13,5 @@ module "vpc" {
 
 resource "aws_internet_gateway" "gw" {
   vpc_id = module.vpc.vpc_id
-  tags = local.tags
+  tags   = local.tags
 }
