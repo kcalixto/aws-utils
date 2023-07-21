3d119fd7a6438d8cf3c881c1e8bc91edb46d6222
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-21 14:15:21 -0300

    test

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "mysql_db_user" {
+  type        = string
   description = "Database username"
 }
 
 variable "mysql_db_pass" {
+  type        = string
   description = "Database password"
 }
