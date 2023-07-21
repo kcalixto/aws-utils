dbbe275b2f93a80e6cb38cc1ec496689f9aa9507
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-20 22:22:38 -0300

    refact vpc

--- provider.tf
+++ provider.tf
@@ -1,3 +1,28 @@
 provider "aws" {
+  region = locals.region
+}
+
+locals {
+  name   = "utils"
   region = "sa-east-1"
+
+  tags = {
+    GithubRepo = "aws-utils"
+  }
+}
+
+################################################################################
+# VPC Module
+################################################################################
+
+module "vpc" {
+  name   = "${local.name}-general-vpc"
+  source = "terraform-aws-modules/vpc/aws"
+
+  cidr               = "10.0.0.0/16"
+  azs                = ["${local.region}a", "${local.region}b"]
+  private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
+  enable_nat_gateway = false
+
+  tags = local.tags
 }
