9367ed142a700ab02c3d4dd7a8c6ece927f56ea7
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-20 22:45:21 -0300

    fix local

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 locals {
-  name   = "utils"
+  name = "utils"
 
   tags = {
     GithubRepo = "aws-utils"
@@ -19,7 +19,7 @@ module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   cidr               = "10.0.0.0/16"
-  azs                = ["${local.region}a", "${local.region}b"]
+  azs                = ["sa-east-1a", "sa-east-1b"]
   private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = false
 
