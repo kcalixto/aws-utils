320db96a40ffac6f5dc7a7fd8bb70318fe021aa3
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-21 21:22:13 -0300

    covering another az

--- vpc.tf
+++ vpc.tf
@@ -89,7 +89,7 @@ module "vpc" {
   cidr               = "10.0.0.0/16"
   azs                = ["sa-east-1a", "sa-east-1b"]
   public_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  database_subnets   = ["10.0.3.0/24"]
+  database_subnets   = ["10.0.3.0/24", "10.0.4.0/24"]
   private_subnets    = ["10.0.101.0/24", "10.0.102.0/24"]
   enable_nat_gateway = false
 
