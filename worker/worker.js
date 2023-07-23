0db2dd3fe7312583f7c1c70230ad0ba4fd270bd8
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-23 15:30:54 -0300

    testing commit

---
+++ worker/worker.js
@@ -0,0 +1,12 @@
+// [ ] initial sls config & deploy
+// [ ] generate link to cancel execution
+// [ ] send slack notification 1 day before asking if should exec
+// [ ] execute terraform destroy
+// [ ] retry at least 3 times after 1h
+// [ ] notify on slack when delete starts
+// [ ] notify on slack when delete is done
+// [ ] notify on slack if delete fails
+
+export async function start() {
+    console.log('started!')
+}
