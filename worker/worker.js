93aa056ba26be9d4c9b56e16c12cd6d25251dba5
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-07-23 22:27:34 -0300

    marked task as done

--- worker/worker.js
+++ worker/worker.js
@@ -1,5 +1,5 @@
 // [X] initial sls config & deploy
-// [ ] generate link to cancel execution
+// [X] generate link to cancel execution
 // [ ] send slack notification 1 day before asking if should exec
 // [ ] execute terraform destroy
 // [ ] retry at least 3 times after 1h
