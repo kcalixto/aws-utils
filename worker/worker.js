e93808be3bfebf0e4c9b2b2de0e1c5283d549d79
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2023-08-29 22:04:56 -0300

    deploy rds

--- worker/worker.js
+++ worker/worker.js
@@ -1,6 +1,6 @@
 // [X] initial sls config & deploy
 // [X] generate link to cancel execution
-// [ ] send slack notification 1 day before asking if should exec
+// [ ] send slack notification 1 day before asking if should not exec
 // [ ] execute terraform destroy
 // [ ] retry at least 3 times after 1h
 // [ ] notify on slack when delete starts
