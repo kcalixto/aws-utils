caf0766fba6f9d70a864b5fe59fb5e40d9c250a0
Kauã Chaves Calixto <80490049+kcalixto@users.noreply.github.com>
2023-10-11 21:38:17 -0300

    hahah (#32)

--- chatbot/chatbot.tf
+++ chatbot/chatbot.tf
@@ -4,8 +4,8 @@ module "chatbot_slack_configuration" {
 
   configuration_name = "chatbot-slack-configuration"
   iam_role_arn       = aws_iam_role.iam_role.arn
-  slack_channel_id   = var.workspace_id
-  slack_workspace_id = var.channel_id
+  slack_channel_id   = var.channel_id
+  slack_workspace_id = var.workspace_id
 
   sns_topic_arns = [
     module.sns_topic.topic_arn,
